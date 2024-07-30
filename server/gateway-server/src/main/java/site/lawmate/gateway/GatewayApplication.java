ee647bd5cc6d4a6b935794eb526d2ab4bce1e704
yangdonggyu8923 <yangdonggyu8923@gmail.com>
2024-07-30 16:59:58 +0900

    lawmate

--- server/gateway-server/src/main/java/site/lawmate/gateway/GatewayApplication.java
+++ server/gateway-server/src/main/java/site/lawmate/gateway/GatewayApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class ApiGatewayApplication {
+public class GatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
+		SpringApplication.run(GatewayApplication.class, args);
 	}
 
 }
